3584fd590531f8a135f4dae53694870a3a5e32de
george-scott-code <george.scott.code@gmail.com>
2023-10-03 21:02:30 +0100

    reveive message from socket listener

--- server/Program.cs
+++ server/Program.cs
@@ -1,3 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
 Console.WriteLine("Hello, World!");
+IPHostEntry ipHostInfo = await Dns.GetHostEntryAsync("host.contoso.com");
+IPAddress ipAddress = ipHostInfo.AddressList[0];
+IPEndPoint ipEndPoint = new(ipAddress, 11_000);
+
+using Socket listener = new(
+    ipEndPoint.AddressFamily,
+    SocketType.Stream,
+    ProtocolType.Tcp);
+
+listener.Bind(ipEndPoint);
+listener.Listen(100);
+
+var handler = await listener.AcceptAsync();
+while (true)
+{
+    // Receive message.
+    var buffer = new byte[1_024];
+    var received = await handler.ReceiveAsync(buffer, SocketFlags.None); 
+    var response = Encoding.UTF8.GetString(buffer, 0, received);
+
+    var eom = "<|EOM|>";
+    if (response.IndexOf(eom) > -1) // is end of message
+    {
+        System.Console.WriteLine(
+            $"Socket server recieved message: {response.Replace(eom, string.Empty)}"
+        );
+    }
+}
