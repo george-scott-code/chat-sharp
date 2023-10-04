36daf7f21c76fc02e2c820753715f4ff11ee87ed
george-scott-code <george.scott.code@gmail.com>
2023-10-04 17:29:55 +0100

    send message

--- client/Program.cs
+++ client/Program.cs
@@ -1,2 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
 Console.WriteLine("Hello, World!");
+
+IPHostEntry ipHostInfo = await Dns.GetHostEntryAsync("localhost");
+IPAddress ipAddress = ipHostInfo.AddressList[0];
+IPEndPoint ipEndPoint = new(ipAddress, 11_000);
+
+using Socket client = new(
+    ipEndPoint.AddressFamily,
+    SocketType.Stream,
+    ProtocolType.Tcp);
+
+await client.ConnectAsync(ipEndPoint);
+while (true)
+{
+    //Send message
+    var message = "Hi friends 👋!<|EOM|>";
+    var messageBytes = Encoding.UTF8.GetBytes(message);
+
+    _ = await client.SendAsync(messageBytes, SocketFlags.None);
+    System.Console.WriteLine($"Socket client sent message {message}");
+}
+
+client.Shutdown(SocketShutdown.Both);
